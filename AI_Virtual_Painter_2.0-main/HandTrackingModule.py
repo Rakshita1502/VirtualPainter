4c0feb3d728262ffb4cc5292d40b45b3a9866381
Rakshita1502 <72328634+Rakshita1502@users.noreply.github.com>
2022-11-17 10:08:30 +0530

    Update HandTrackingModule.py

--- AI_Virtual_Painter_2.0-main/HandTrackingModule.py
+++ AI_Virtual_Painter_2.0-main/HandTrackingModule.py
@@ -12,10 +12,12 @@ class handDetector():
         self.modelComplex = modelComplexity
         self.trackCon = trackCon
 
+        # mediapipe function for tracking hand in real time
         self.mpHands = mp.solutions.hands
 
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex,
                                         self.detectionCon, self.trackCon)
+        # mediapipe function for drawing lines between the points connected on hand
         self.mpDraw = mp.solutions.drawing_utils
         self.tipIds = [4, 8, 12, 16, 20]
 
@@ -59,7 +61,7 @@ class handDetector():
     def fingersUp(self):
         fingers = []
         # Thumb
-        if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0] - 1][1]:
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
@@ -75,19 +77,7 @@ class handDetector():
 
         return fingers
 
-    def findDistance(self, p1, p2, img, draw=True,r=15, t=3):
-        x1, y1 = self.lmList[p1][1:]
-        x2, y2 = self.lmList[p2][1:]
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
-        if draw:
-            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
-            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
-            length = math.hypot(x2 - x1, y2 - y1)
-
-        return length, img, [x1, y1, x2, y2, cx, cy]
 
 def main():
     pTime = 0
@@ -105,8 +95,8 @@ def main():
         fps = 1 / (cTime - pTime)
         pTime = cTime
 
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-        (255, 0, 255), 3)
+        # cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+        # (255, 0, 255), 3)
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
